76bf9408a9ba32483c31197899cf94be16003c78
Eduardo Valdivia <eddval0808@gmail.com>
2024-04-03 11:02:58 -0600

    update gitignore and run script

--- projects/PA2/run.sh
+++ projects/PA2/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 # -n num msg  -z seq num limit   -d avg. time between   -l loss -c corruption -s random seed -v trace
-python3 rdtsim.py -n 1000 -z 2 -d 10 -l 0.0 -c 0.0 -s 3838383838 -v 1 > out.txt
-bat out.txt
+echo "first run\n" > out.txt
+python3 rdtsim.py -n 1000 -z 2 -d 100 -l 0.1 -c 0.0 -v 0 >> out.txt
+echo "sec run\n" >> out.txt
+python3 rdtsim.py -n 1000 -z 2 -d 100 -l 0.0 -c 0.1 -v 0 >> out.txt
+echo "thir run\n" >> out.txt
+python3 rdtsim.py -n 1000 -z 2 -d 100 -l 0.05 -c 0.1 -v 0 >> out.txt
+echo "four run\n" >> out.txt
+python3 rdtsim.py -n 1000 -z 2 -d 100 -l 0.1 -c 0.05 -v 0 >> out.txt
